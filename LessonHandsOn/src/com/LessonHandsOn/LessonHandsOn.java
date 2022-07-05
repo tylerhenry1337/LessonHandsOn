55c337886b0230bbf9b0c6e426cd05af6d7c7f13
Tyler <75041195+tylerhenry1337@users.noreply.github.com>
2022-07-05 14:10:53 -0400

    Add files via upload

---
+++ LessonHandsOn/src/com/LessonHandsOn/LessonHandsOn.java
@@ -0,0 +1,16 @@
+package com.LessonHandsOn;
+
+public class LessonHandsOn {
+    int count;
+    boolean flag;
+    public boolean isTrue(int count){
+        if(count >  4){
+            return flag= true;
+        }
+        else{
+            return flag = false;
+        }
+
+    }
+
+}
---
+++ LessonHandsOn/src/com/LessonHandsOn/TDD.java
@@ -0,0 +1,17 @@
+package com.LessonHandsOn;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TDD {
+    @Test
+    public void testIsTrue(){
+        LessonHandsOn lho = new LessonHandsOn();
+        Assert.assertEquals(true, lho.isTrue(5));
+
+    }
+
+
+
+
+}
